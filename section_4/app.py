6ea235adaff7bb90ff2eab99af7c248b63e4afb1
OdilonJK <kroger.odilonj@gmail.com>
2018-12-02 23:14:03 -0200

    Refactored parser and item getter

--- section_4/app.py
+++ section_4/app.py
@@ -23,22 +23,26 @@ items = [
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', 
+        type=float, 
+        required=True,
+        help='This field cannot be left blank.'
+    )
+
+    def get_item(self, name):
+        return next(filter(lambda i: i['name'] == name, items), None)
+
     @jwt_required()
     def get(self, name):
-        item = next(filter(lambda i: i['name'] == name, items), None)
+        item = Item.get_item(self, name)
         return {'item': item}, 200 if item else 404
 
     @jwt_required()
     def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', 
-            type=float, 
-            required=True,
-            help='This field cannot be left blank.'
-        )
-        data = parser.parse_args()
-        if next(filter(lambda i: i['name'] == name, items), None) is not None:
+        if Item.get_item(self, name) is not None:
             return {'message': 'An item called {} already exists.'.format(name)}, 400
+        data = Item.parser.parse_args()
         new_item = {'name': name, 'price': data['price']}
         items.append(new_item)
         return new_item, 201
@@ -46,21 +50,15 @@ class Item(Resource):
     @jwt_required()
     def delete(self, name):
         global items
-        if (next(filter(lambda i: i['name'] == name, items), None)) is None:
+        if Item.get_item(self, name) is None:
             return {'message': 'There is no item called {}.'.format(name)}, 404
         items = [i for i in items if i['name'] != name]
         return {'message': '{} deleted.'.format(name)}
 
     @jwt_required()
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', 
-            type=float, 
-            required=True,
-            help='This field cannot be left blank.'
-        )
-        data = parser.parse_args()
-        item = next(filter(lambda i: i['name'] == name, items), None)
+        data = Item.parser.parse_args()
+        item = Item.get_item(self, name)
         if item is None:
             new_item = {'name': name, 'price': data['price']}
             items.append(new_item)
