028f0da02e2074232b4941c8766b15d59dca5055
OdilonJK <kroger.odilonj@gmail.com>
2018-12-12 19:15:29 -0200

    Added user register endpoint + create_table.py

--- section_5/user.py
+++ section_5/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -37,3 +38,33 @@ class User:
         
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', 
+        type=str, 
+        required=True,
+        help='This field cannot be left blank.'
+    )
+    parser.add_argument('password', 
+        type=str, 
+        required=True,
+        help='This field cannot be left blank.'
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        username = data['username']
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'INSERT INTO users VALUES (NULL, ?, ?)'
+        
+        cursor.execute(query, (username, data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'User {} created successfully.'.format(username)}, 201
